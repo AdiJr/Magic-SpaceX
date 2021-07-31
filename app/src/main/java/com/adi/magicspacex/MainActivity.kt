5b553ca1e5185d8a79aca73d53b68604a94969f9
Adrian Stasiak <adrian.stasiak@accenture.com>
2021-07-31 19:43:16 +0200

    Added basic implementation for splash screen

--- app/src/main/java/com/adi/magicspacex/MainActivity.kt
+++ app/src/main/java/com/adi/magicspacex/MainActivity.kt
@@ -3,21 +3,15 @@ package com.adi.magicspacex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import coil.annotation.ExperimentalCoilApi
 import com.adi.magicspacex.ui.MyApp
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalCoilApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp()
         }
     }
-}
-
-@Preview("HomeScreen", showBackground = true, showSystemUi = true)
-@Composable
-fun DefaultPreview() {
-    MyApp()
 }
