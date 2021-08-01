c2f7d2c3fb3b17e34ba0a770c09641fdbf23cfda
Adrian Stasiak <adrian.stasiak@accenture.com>
2021-08-01 19:46:21 +0200

    Added libraries, extracted libs versions to buildSrc module, updated gradle

--- app/src/main/java/com/adi/magicspacex/ui/SplashScreen.kt
+++ app/src/main/java/com/adi/magicspacex/ui/SplashScreen.kt
@@ -1,62 +1,15 @@
 package com.adi.magicspacex.ui
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import coil.annotation.ExperimentalCoilApi
-import coil.compose.ImagePainter
-import coil.compose.rememberImagePainter
 import com.adi.magicspacex.utils.theme.MainAppTheme
-import java.util.*
 
-@ExperimentalCoilApi
 @Composable
 fun MyApp() {
     MainAppTheme {
-        val scaffoldState = rememberScaffoldState()
-        Scaffold(
-            scaffoldState = scaffoldState,
-            backgroundColor = Color.White
-        ) {
-            val painter = rememberImagePainter(
-                "https://images.unsplash.com/photo-1627680342808-eabaa8cbad88?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
-                builder = {
-                    crossfade(true)
-                })
-
-            when (painter.state) {
-                is ImagePainter.State.Loading -> CircularProgressIndicator()
-                is ImagePainter.State.Error -> Text(text = "Error")
-                else -> Box {
-                    Image(
-                        modifier = Modifier.fillMaxSize(), contentScale = ContentScale.FillHeight,
-                        painter = painter,
-                        contentDescription = null,
-                    )
-                    Text(
-                        modifier = Modifier
-                            .align(Alignment.BottomCenter)
-                            .padding(bottom = 100.dp),
-                        text = "Magic Spacex".uppercase(Locale.getDefault()),
-                        style = TextStyle(
-                            color = Color.White, fontSize = 35.sp
-                        )
-                    )
-                }
-            }
+        Surface {
+            Text("Hello Splash")
         }
     }
 }
---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,87 @@
+package com.adi.magicspacex.buildsrc
+
+object Versions {
+    const val ktLint = "0.41.0"
+}
+
+object Libs {
+    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0"
+    const val ktLint = "com.pinterest:ktlint:${Versions.ktLint}"
+
+    object Accompanist {
+        private const val version = "0.15.0"
+        const val insets = "com.google.accompanist:accompanist-insets:$version"
+    }
+
+    object Kotlin {
+        private const val version = "1.5.10"
+        const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
+        const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
+        const val extensions = "org.jetbrains.kotlin:kotlin-android-extensions:$version"
+
+        object Coroutines {
+            private const val version = "1.5.0"
+            const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
+            const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$version"
+        }
+    }
+
+    object AndroidX {
+        object Activity {
+            const val activityCompose = "androidx.activity:activity-compose:1.3.0"
+        }
+
+        const val appcompat = "androidx.appcompat:appcompat:1.3.0"
+
+        object Compose {
+            const val version = "1.0.0"
+
+            const val runtime = "androidx.compose.runtime:runtime:$version"
+            const val runtimeLivedata = "androidx.compose.runtime:runtime-livedata:$version"
+            const val material = "androidx.compose.material:material:$version"
+            const val foundation = "androidx.compose.foundation:foundation:$version"
+            const val layout = "androidx.compose.foundation:foundation-layout:$version"
+            const val tooling = "androidx.compose.ui:ui-tooling:$version"
+            const val animation = "androidx.compose.animation:animation:$version"
+            const val uiTest = "androidx.compose.ui:ui-test-junit4:$version"
+        }
+
+        object Lifecycle {
+            private const val version = "2.3.1"
+            const val viewModelCompose =
+                "androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07"
+            const val viewModelKtx = "androidx.lifecycle:lifecycle-viewmodel-ktx:$version"
+        }
+
+        object Test {
+            private const val version = "1.3.0"
+            const val runner = "androidx.test:runner:$version"
+            const val rules = "androidx.test:rules:$version"
+
+            object Ext {
+                private const val version = "1.1.2"
+                const val junit = "androidx.test.ext:junit-ktx:$version"
+            }
+
+            const val espressoCore = "androidx.test.espresso:espresso-core:3.2.0"
+        }
+    }
+
+    object Hilt {
+        private const val version = "2.37"
+
+        const val gradlePlugin = "com.google.dagger:hilt-android-gradle-plugin:$version"
+        const val android = "com.google.dagger:hilt-android:$version"
+        const val compiler = "com.google.dagger:hilt-compiler:$version"
+        const val testing = "com.google.dagger:hilt-android-testing:$version"
+    }
+
+    object JUnit {
+        private const val version = "4.13"
+        const val junit = "junit:junit:$version"
+    }
+
+    object Coil {
+        const val coilCompose = "io.coil-kt:coil-compose:1.3.0"
+    }
+}
