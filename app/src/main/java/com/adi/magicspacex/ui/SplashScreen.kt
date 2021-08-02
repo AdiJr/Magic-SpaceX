f4d895c00b8ee13d1fb077db0508ebd22c9ec48c
Adi <42969638+adijr@users.noreply.github.com>
2021-08-02 21:19:23 +0200

    Added onTimeout to splash

--- app/src/main/java/com/adi/magicspacex/ui/SplashScreen.kt
+++ app/src/main/java/com/adi/magicspacex/ui/SplashScreen.kt
@@ -3,17 +3,29 @@ package com.adi.magicspacex.ui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Modifier
 import com.adi.magicspacex.R
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.LottieConstants
 import com.airbnb.lottie.compose.rememberLottieComposition
+import kotlinx.coroutines.delay
+
+private const val SplashWaitTime: Long = 5000
 
 @Composable
-fun SplashScreen() {
+fun SplashScreen(onTimeout: () -> Unit) {
     val loadingTextComposition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.loading_text))
+
+    val currentOnTimeout by rememberUpdatedState(onTimeout)
+
+    LaunchedEffect(Unit) {
+        delay(SplashWaitTime)
+        currentOnTimeout()
+    }
 
     Column(Modifier.fillMaxSize()) {
         LottieAnimation(
