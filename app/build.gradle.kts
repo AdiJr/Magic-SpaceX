397aafc11400c3206c917e761df6f48af4da1b60
AdiJr <andyguitarist18@gmail.com>
2025-05-16 12:19:35 +0200

    Updated dependencies and target, compile SDKs to API 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,13 +10,13 @@ plugins {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 35
     namespace = "com.adi.magicspacex"
 
     defaultConfig {
         applicationId = "com.adi.magicspacex"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/adi/magicspacex/utils/model/helpers/State.kt
+++ app/src/main/java/com/adi/magicspacex/utils/model/helpers/State.kt
@@ -2,32 +2,38 @@ package com.adi.magicspacex.utils.model.helpers
 
 /**
  * State holder acquiring states Idle, Loading or Error.
+ *
+ * This sealed class represents the different states an operation or data fetching process can be in.
+ * It extends `DataState<Nothing>` as it doesn't directly hold data itself, but rather indicates
+ * the overall status.
  */
 sealed class State : DataState<Nothing>() {
 
     /**
-     * Nothing to do.
+     * Represents an idle state where no operation is in progress.
      */
-    object Idle : State()
+    data object Idle : State()
 
     /**
-     * Data is loading.
+     * Represents a loading state where an operation is currently in progress.
      */
-    object Loading : State()
+    data object Loading : State()
 
     /**
-     * Some error has happened.
+     *  Signifies that an error has occurred during the operation, and it contains the
+     *  [Throwable] that caused the error.
      */
     data class Error(val error: Throwable) : State()
 }
 
 /**
- * State data holder which includes also [State] states.
+ * Represents the state of data, which can be either loaded, loading, idle, or an error.
+ * This sealed class is designed to be used in conjunction with [State] to provide a comprehensive
+ * representation of data states in an application.
+ *
+ * @param T The type of data held by the [Loaded] state.
  */
 sealed class DataState<out T> {
 
-    /**
-     * Some data are loaded.
-     */
     data class Loaded<out T>(val data: T) : DataState<T>()
 }
